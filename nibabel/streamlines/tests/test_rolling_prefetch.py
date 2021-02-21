6f82bf2dda1fc8b9a072cd92e79d1a015a6acab6
valhayot <valeriehayot@gmail.com>
2021-02-21 12:58:41 +0000

    fixed test formatting

--- nibabel/streamlines/tests/test_rolling_prefetch.py
+++ nibabel/streamlines/tests/test_rolling_prefetch.py
@@ -11,8 +11,8 @@ from nibabel.streamlines import rolling_prefetch as rp
 
 
 CACHE_DIR = "/dev/shm"
-CACHE_SIZE = 1024**2
-CACHES = { CACHE_DIR : CACHE_SIZE }
+CACHE_SIZE = 1024 ** 2
+CACHES = {CACHE_DIR: CACHE_SIZE}
 DELETE_STR = ".nibtodelete"
 BUCKET_NAME = "s3trk"
 
@@ -44,6 +44,7 @@ def s3_base():
     proc.terminate()
     proc.wait()
 
+
 @pytest.fixture()
 def s3(s3_base):
     from botocore.session import Session
@@ -54,6 +55,7 @@ def s3(s3_base):
     client.create_bucket(Bucket=BUCKET_NAME)
 
     yield
+
 
 @pytest.fixture
 def create_main_file(s3):
@@ -66,10 +68,11 @@ def create_main_file(s3):
     s3_path = os.path.join(BUCKET_NAME, fname)
     fs = s3fs.S3FileSystem()
 
-    with fs.open(s3_path, 'wb') as f:
+    with fs.open(s3_path, "wb") as f:
         f.write(data)
 
     return s3_path
+
 
 @pytest.fixture
 def create_cached(create_main_file):
@@ -83,22 +86,29 @@ def create_cached(create_main_file):
     cfname_0 = os.path.join(CACHE_DIR, f"{fname}.0")
     cfname_1 = os.path.join(CACHE_DIR, f"{fname}.{csize}")
 
-    with open(cfname_0, 'wb') as cf_:
+    with open(cfname_0, "wb") as cf_:
         cf_.write(f.read(csize))
 
-    with open(cfname_1, 'wb') as cf_:
+    with open(cfname_1, "wb") as cf_:
         cf_.write(f.read(csize))
 
     f.seek(0, os.SEEK_SET)
 
     cf_ = open(cfname_0, "rb")
 
-    return { "f": f, "nbytes": csize*2 + 256, "fn_prefix": fname, "cf_": cf_, "fidx": (0, csize) }
+    return {
+        "f": f,
+        "nbytes": csize * 2 + 256,
+        "fn_prefix": fname,
+        "cf_": cf_,
+        "fidx": (0, csize),
+    }
 
 
 def cleanup(fn_prefix):
     for c in Path(CACHE_DIR).glob(fn_prefix + "*"):
         c.unlink()
+
 
 def test_prefetch(create_main_file):
     fname = create_main_file
@@ -111,26 +121,26 @@ def test_prefetch(create_main_file):
     # test prefetching
     cached_files = Path(CACHE_DIR).glob("*")
     cf = list(cached_files)
-    assert(len(cf) == 2)
+    assert len(cf) == 2
 
     # file removal
     to_remove = os.path.join(CACHE_DIR, f"{f_bn}.1{DELETE_STR}")
     os.rename(cf[0], to_remove)
 
     rp.prefetch(fname, CACHES, block_size=CACHE_SIZE)
-    assert(not os.path.exists(to_remove))
+    assert not os.path.exists(to_remove)
     cleanup(f_bn)
-    
-    
+
+
 def test_cached_read(create_cached):
 
     data, cf_, fidx = rp.cached_read(caches=CACHES, **create_cached)
     create_cached["f"].seek(0, os.SEEK_SET)
 
-    assert(data == create_cached["f"].read(create_cached["nbytes"]))
-    assert(cf_ is None)
-    assert(fidx is None)
-    assert(os.path.exists(f"{create_cached['cf_'].name}{DELETE_STR}"))
+    assert data == create_cached["f"].read(create_cached["nbytes"])
+    assert cf_ is None
+    assert fidx is None
+    assert os.path.exists(f"{create_cached['cf_'].name}{DELETE_STR}")
 
     cleanup(create_cached["fn_prefix"])
 
@@ -140,10 +150,10 @@ def test_pf_read_cached(create_cached):
     data, cf_, fidx = rp.pf_read(caches=CACHES, **create_cached)
     create_cached["f"].seek(0, os.SEEK_SET)
 
-    assert(data == create_cached["f"].read(create_cached["nbytes"]))
-    assert(cf_ is None)
-    assert(fidx is None)
-    assert(os.path.exists(f"{create_cached['cf_'].name}{DELETE_STR}"))
+    assert data == create_cached["f"].read(create_cached["nbytes"])
+    assert cf_ is None
+    assert fidx is None
+    assert os.path.exists(f"{create_cached['cf_'].name}{DELETE_STR}")
 
     cleanup(create_cached["fn_prefix"])
 
@@ -152,15 +162,16 @@ def test_pf_read_uncached(create_main_file):
 
     fn_prefix = os.path.basename(create_main_file)
     fs = s3fs.S3FileSystem()
-    nbytes=1024
+    nbytes = 1024
 
-    with fs.open(create_main_file, 'rb') as f:
-        data, cf_, fidx = rp.pf_read(caches=CACHES, f=f, nbytes=1024, fn_prefix=fn_prefix)
+    with fs.open(create_main_file, "rb") as f:
+        data, cf_, fidx = rp.pf_read(
+            caches=CACHES, f=f, nbytes=1024, fn_prefix=fn_prefix
+        )
         f.seek(0, os.SEEK_SET)
-        assert(data == f.read(nbytes))
+        assert data == f.read(nbytes)
 
-    assert(cf_ is None)
-    assert(fidx is None)
+    assert cf_ is None
+    assert fidx is None
 
     cleanup(fn_prefix)
-
